0e296b60c2ac43bfce171d18847b4ab9599a6baf
Weslan R. Alves <weslan.rezende@gmail.com>
2020-01-07 23:15:55 -0300

    Purge cache

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import { perfil } from './config/enumerate';
 
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import Login from './pages/Login';
+// import Login from './pages/Login';
 
 // Páginas de coordenador
 import CDT_Atividade from './pages/atividade/cadastrar';
@@ -63,7 +63,7 @@ const PrivateLaboratorio = ({ component: Component, perfil: perfilUser, ...rest 
 const Routes = props => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ Login } />
+      {/* <Route exact path="/" component={ Login } /> */}
 
       {/* Rotas de coordenador */}
       <PrivateCoordenador path="/trabalho_diario/iniciar" component={CDT_Trabalho_diario} perfil={ props.user.perfil } />

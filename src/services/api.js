4ac920bbbe64fae80fe4e70f30f0929a1524b202
Weslan R. Alves <weslan.rezende@gmail.com>
2020-01-27 23:22:52 -0300

    configurando .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+require('dotenv').config();
 
 const api = axios.create({
-  baseURL: process.env.APP_URL || 'http://localhost:3333'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333'
 });
 
 export default api;

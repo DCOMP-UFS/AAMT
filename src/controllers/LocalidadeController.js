98246a53499d9d896eb17b4ef812c087c7362dd7
Weslan R. Alves <weslan.rezende@gmail.com>
2020-01-30 17:27:17 -0300

    Incluindo método PUT para atualização das localidades

--- src/controllers/LocalidadeController.js
+++ src/controllers/LocalidadeController.js
@@ -77,11 +77,71 @@ store = async (req, res) => {
   return res.status(201).json(user);
 }
 
+update = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.userId;
+  const { categoria_id, municipio_id } = req.body;
+
+  const coordinator = await isCoordinator( userId );
+  if( !coordinator ) {
+    return res.status(403).json({ error: 'Acesso negado' });
+  }
+
+  const localidade = await Localidade.findByPk( id );
+  if( !localidade ) {
+    return res.status(400).json({ error: 'Localidade não encontrada' });
+  }
+
+  if( categoria_id ) {
+    const categoria = await Categoria.findByPk( categoria_id );
+    if( !categoria ) {
+      return res.status(400).json({ error: 'Categoria não encontrada' });
+    }
+  }
+
+  if( municipio_id ) {
+    const municipio = await Municipio.findByPk( municipio_id );
+    if( !municipio ) {
+      return res.status(400).json({ error: 'Município não encontrada' });
+    }
+  }
+
+  req.body.id = undefined;
+  req.body.createdAt = undefined;
+  req.body.updatedAt = undefined;
+
+  const { isRejected } = await Localidade.update(
+    req.body,
+    {
+      where: {
+        id
+      }
+    }
+  );
+
+  if( isRejected ){
+    return res.status(400).json({ error: 'Não foi possível atualizar usuário' });
+  }
+
+  const result = await Localidade.findByPk( id,  {
+    include: [
+      { association: 'categoria', attributes: { exclude: [ 'createdAt', 'updatedAt' ] } }, 
+      { association: 'municipio', attributes: { exclude: [ 'createdAt', 'updatedAt' ] } }
+    ],
+    attributes: {
+      exclude: [ 'categoria_id', 'municipio_id' ]
+    }
+  });
+
+  return res.json( result );
+}
+
 const router = express.Router();
 router.use(authMiddleware);
 
 router.get('/', index);
 router.get('/:categoria_id/categorias', listByCategory);
 router.post('/:categoria_id/categorias/:municipio_id/municipios', store);
+router.put('/:id', update);
 
 module.exports = app => app.use('/localidades', router);

7d6cf2630860e5d22aab6bb16d4a008e2b7e316b
AVONALE\weslan.alves <weslan.alves@napista.com.br>
2021-01-07 22:53:42 -0300

    Criando rota para consultar as vistorias pelo id do trabalho diário

--- src/controllers/VistoriaController.js
+++ src/controllers/VistoriaController.js
@@ -110,6 +110,108 @@ getInspects = async ( req, res ) => {
   });
 }
 
+getInspectsByDailyWork = async ( req, res ) => {
+  const { trabalho_diario_id } = req.params;
+  const userId = req.userId;
+
+  // Iniciando validação
+  const userRequest = await Usuario.findByPk( userId, {
+    include: {
+      association: "atuacoes"
+    }
+  });
+
+  let fl_agente = false;
+  userRequest.atuacoes.forEach( at => {
+    if( at.tipoPerfil === 4 )
+      fl_agente = true;
+  });
+
+  if( fl_agente && parseInt( usuario_id ) !== userRequest.id )
+    return res.status(400).json({ error: "Acesso negado" });
+
+  const trabalho_diario = await TrabalhoDiario.findByPk( trabalho_diario_id );
+
+  if( !trabalho_diario )
+    return res.status(400).json({ error: "Trabalho diário não existe" });
+  // Fim validação
+
+  const vistorias = await Vistoria.findAll({
+    order: [
+      [ 'createdAt', 'DESC' ]
+    ],
+    include: [
+      {
+        where: {
+          trabalho_diario_id: trabalho_diario.id
+        },
+        association: 'trabalhoDiario',
+        attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+        include: {
+          association: 'equipe',
+          attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+          include: {
+            association: 'atividade',
+            attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+            include: [
+              {
+                association: 'ciclo',
+                attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+              },
+              {
+                association: 'metodologia',
+                attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+              },
+              {
+                association: 'objetivo',
+                attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+              }
+            ]
+          }
+        }
+      },
+      {
+        association: 'imovel',
+        attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+        include: {
+          association: 'lado',
+          attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+          include: [
+            {
+              association: 'quarteirao',
+              attributes: { exclude: [ 'createdAt', 'updatedAt' ] }
+            },
+            {
+              association: 'rua',
+              attributes: { exclude: [ 'createdAt', 'updatedAt' ] }
+            }
+          ]
+        }
+      },
+      {
+        association: 'depositos',
+        attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+        include: [
+          {
+            association: 'tratamentos',
+            attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+          },
+          {
+            association: 'amostras',
+            attributes: { exclude: [ 'createdAt', 'updatedAt' ] },
+          }
+        ]
+      }
+    ]
+  });
+
+  return res.json({
+    status: 'success',
+    mensage: '',
+    data: vistorias
+  });
+}
+
 getInspectsByPeriod = async ( req, res ) => {
   const { usuario_id, data } = req.params;
   const userId = req.userId;
@@ -166,6 +268,7 @@ const router = express.Router();
 router.use( authMiddleware );
 
 router.get('/:usuario_id/usuarios', getInspects);
+router.get('/:trabalho_diario_id/trabalhos_diarios', getInspects);
 router.get('/periodo/:usuario_id/usuarios/:data_inicio/data_inicio/:data_fim/data_fim', getInspectsByPeriod);
 
 module.exports = app => app.use('/vistorias', router);

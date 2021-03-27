131b642f5064442745032befa998360293f15bae
Adam Araújo <adamcsaraujo@gmail.com>
2021-03-27 01:43:11 -0300

    #74 - Algoritmo parcialmente finalizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useSelector, connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import jwtDecode from 'jwt-decode';
+
+import { signOut } from './store/modules/auth/actions';
+
+import api from './services/api';
 
 import Routes from './routes';
 
-const App = () => {
+const App = ({ token, ...props }) => {
+  api.interceptors.request.use(
+    function (config) {
+      if (token) {
+        const { exp } = jwtDecode(token);
+
+        const expirationTime = exp * 1000 - 60000;
+
+        if (Date.now() >= expirationTime) {
+          props.signOut();
+        }
+
+        // console.log(expirationTime);
+      }
+
+      return config;
+    },
+    function (error) {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  );
+
   return <Routes />;
 };
 
-export default App;
+const mapStateToProps = state => ({
+  token: state.auth.token,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      signOut,
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

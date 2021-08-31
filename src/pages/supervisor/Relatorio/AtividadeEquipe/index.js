dd738af0827db11b9ebb7ce35198c6ffbc4f47d6
Adam <adamcsaraujo@gmail.com>
2021-08-31 11:09:18 -0300

    #117 - Mudança no título do relatório

--- src/pages/supervisor/Relatorio/AtividadeEquipe/index.js
+++ src/pages/supervisor/Relatorio/AtividadeEquipe/index.js
@@ -35,7 +35,7 @@ export const RelatorioAtividadeEquipe = ({ atividades, ciclo, usuario, ...props 
       <PageHeader>
         <h3 className="page-title">
           <PageIcon><FaChartLine /></PageIcon>
-          Relatório Diário por Equipe
+          Relatório da Atividade por Equipe
         </h3>
       </PageHeader>
 

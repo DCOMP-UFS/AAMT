2ab22fb42a11808926da43879ae9fb9888d543d0
rafae9000 <42677838+rafae9000@users.noreply.github.com>
2022-11-17 17:14:08 -0300

    Melhoria metodologiaSagas
    Melhora das mensagens de error, indicando se a falha ocorreu devido a um problema na api ou se é problema de conexão com a internet

--- src/store/Metodologia/metodologiaSagas.js
+++ src/store/Metodologia/metodologiaSagas.js
@@ -13,8 +13,15 @@ export function* getMethodologies(action) {
       yield put( AppConfigActions.showNotifyToast( "Falha ao consultar metodologias: " + status, "error" ) );
     }
 
-  } catch (error) {
-    yield put( AppConfigActions.showNotifyToast( "Erro ao consultar metodologias, favor verifique a conexão", "error" ) );
+  } catch (err) {
+    if(err.response){
+      //Provavel erro de logica na API
+      yield put( AppConfigActions.showNotifyToast( "Erro ao consultar metodologias, entre em contato com o suporte", "error" ) );
+      
+    }
+    //Se chegou aqui, significa que não houve resposta da API
+    else
+      yield put( AppConfigActions.showNotifyToast( "Erro ao consultar metodologias, favor verifique a conexão", "error" ) );
   }
 }
 

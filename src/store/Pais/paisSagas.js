f209eb011de2d2266f15a8bb4f930ce0bb177bfa
rafae9000 <42677838+rafae9000@users.noreply.github.com>
2022-11-17 17:41:52 -0300

    Melhoria de paisSagas
    Melhora das mensagens de error, indicando se a falha ocorreu devido a um problema na api ou se é problema de conexão com a internet

--- src/store/Pais/paisSagas.js
+++ src/store/Pais/paisSagas.js
@@ -13,8 +13,15 @@ export function* getNations(action) {
       yield put( AppConfigActions.showNotifyToast( "Falha ao consultar países: " + status, "error" ) );
     }
 
-  } catch (error) {
-    yield put( AppConfigActions.showNotifyToast( "Erro ao consultar os países, favor verifique a conexão", "error" ) );
+  } catch (err) {
+    if(err.response){
+      //Provavel erro de logica na API
+      yield put( AppConfigActions.showNotifyToast( "Erro ao consultar países, entre em contato com o suporte", "error" ) );
+      
+    }
+    //Se chegou aqui, significa que não houve resposta da API
+    else
+      yield put( AppConfigActions.showNotifyToast( "Erro ao consultar países, favor verifique a conexão", "error" ) );
   }
 }
 

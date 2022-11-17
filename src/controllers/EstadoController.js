5e3ab97232a416928c46aa384350f9e3032dc49f
rafae9000 <42677838+rafae9000@users.noreply.github.com>
2022-11-17 14:41:42 -0300

    Melhoria nas rotas de estado
    Caso ocorresse algum comportamento inesperado em alguma das rotas, não conseguir conectar com o banco por exemplo, ela não era tratado por uma clausula try/catch, o que fazia a api parar de funcionar.
    
    Agora quando uma falha inesperada acontecer, é enviado uma reposta para o web: "Algum problema inesperado ocorreu nesta rota da api".

--- src/controllers/EstadoController.js
+++ src/controllers/EstadoController.js
@@ -8,50 +8,71 @@ const { Op } = require("sequelize");
 const allowFunction = require('../util/allowFunction');
 
 index = async ( req, res ) => {
-  const estados = await Estado.findAll({
-    order: [[ 'nome', 'asc' ]]
-  });
+  try{
+    const estados = await Estado.findAll({
+      order: [[ 'nome', 'asc' ]]
+    });
 
-  return res.json( estados );
+    return res.json( estados );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 getById = async ( req, res ) => {
-  const { id } = req.params;
+  try{
+    const { id } = req.params;
 
-  const estado = await Estado.findByPk( id, {
-    include: {
-      association: 'regiao'
-    },
-    attributes: {
-      exclude: 'regiao_id',
-      include: 'id'
+    const estado = await Estado.findByPk( id, {
+      include: {
+        association: 'regiao'
+      },
+      attributes: {
+        exclude: 'regiao_id',
+        include: 'id'
+      }
+    });
+
+    if( !estado ) {
+      return res.status(400).json({ error: "Estado não existe" });
     }
-  });
 
-  if( !estado ) {
-    return res.status(400).json({ error: "Estado não existe" });
+    return res.json( estado );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  return res.json( estado );
 }
 
 getStatesByRegion = async ( req, res ) => {
-  const { regiao_id } = req.params;
+  try{
+    const { regiao_id } = req.params;
 
-  const regiao = Regiao.findByPk( regiao_id );
+    const regiao = Regiao.findByPk( regiao_id );
 
-  if( !regiao ) {
-    return res.status(400).json({ error: "Região não existe" });
+    if( !regiao ) {
+      return res.status(400).json({ error: "Região não existe" });
+    }
+
+    const estados = await Estado.findAll({
+      where: {
+        regiao_id
+      },
+      order: [[ 'nome', 'asc' ]]
+    });
+
+    return res.json( estados );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  const estados = await Estado.findAll({
-    where: {
-      regiao_id
-    },
-    order: [[ 'nome', 'asc' ]]
-  });
-
-  return res.json( estados );
 }
 
 
@@ -75,35 +96,42 @@ getStatesByRegion = async ( req, res ) => {
 // }
 
 store = async (req, res) => {
-  const { nome, sigla, regiao_id } = req.body;
-  const userId = req.userId;
+  try{
+    const { nome, sigla, regiao_id } = req.body;
+    const userId = req.userId;
 
-  const allow = await allowFunction( req.userId, 'manter_estado' );
+    const allow = await allowFunction( req.userId, 'manter_estado' );
 
-  if( !allow ) {
-    return res.status(403).json({ error: 'Acesso negado' });
-  }
-
-  const isUnique = await Estado.findOne({
-    where: {
-      [Op.or]: [
-        {nome},
-        {sigla}
-      ]
+    if( !allow ) {
+      return res.status(403).json({ error: 'Acesso negado' });
     }
-  });
-  
-  if( isUnique ) {
-    return res.status(400).json({ error: 'Nome ou sigla do estado já existe' });
-  }
-   
-  const estado = await Estado.create({ 
-    nome,
-    sigla,
-    regiao_id
-  });
 
-  return res.status(201).json( estado );
+    const isUnique = await Estado.findOne({
+      where: {
+        [Op.or]: [
+          {nome},
+          {sigla}
+        ]
+      }
+    });
+    
+    if( isUnique ) {
+      return res.status(400).json({ error: 'Nome ou sigla do estado já existe' });
+    }
+    
+    const estado = await Estado.create({ 
+      nome,
+      sigla,
+      regiao_id
+    });
+
+    return res.status(201).json( estado );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 const router = express.Router();

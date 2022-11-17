144685ef2e21d670f0fb1caf1986323b43f403d6
rafae9000 <42677838+rafae9000@users.noreply.github.com>
2022-11-17 11:27:02 -0300

    Melhoria nas rotas de categoria
    Caso ocorresse algum comportamento inesperado em alguma das rotas de amostrar, não conseguir conectar com o banco por exemplo, ela não era tratado por uma clausula try/catch, o que fazia a api parar de funcionar.
    
    Agora quando uma falha inesperada acontecer, é enviado uma reposta para o web: "Algum problema inesperado ocorreu nesta rota da api".

--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -7,60 +7,81 @@ const Localidade = require('../models/Localidade');
 const allowFunction = require('../util/allowFunction');
 
 index = async ( req, res ) => {
-  const categorias = await Categoria.findAll({
-    order: [[ "nome", "asc" ]]
-  });
+  try{
+    const categorias = await Categoria.findAll({
+      order: [[ "nome", "asc" ]]
+    });
 
-  res.json( categorias );
+    res.json( categorias );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 store = async (req, res) => {
-  const { nome, descricao } = req.body;
+  try{
+    const { nome, descricao } = req.body;
 
-  const categoria = await Categoria.create({ 
-    nome,
-    descricao,
-    ativo: 1
-  });
+    const categoria = await Categoria.create({ 
+      nome,
+      descricao,
+      ativo: 1
+    });
 
-  return res.status(201).json(categoria);
+    return res.status(201).json(categoria);
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 update = async (req, res) => {
-  const userId = req.userId;
-  const { id } = req.params;
+  try{
+    const userId = req.userId;
+    const { id } = req.params;
 
-  const allow = await allowFunction( req.userId, 'manter_categoria' );
+    const allow = await allowFunction( req.userId, 'manter_categoria' );
 
-  if( !allow ) {
-    return res.status(403).json({ error: 'Acesso negado' });
-  }
-
-  const categoria = await Categoria.findByPk( id );
-  if( !categoria ) {
-    return res.status(400).json({ error: 'Está categoria não existe' });
-  }
-
-  req.body.id = undefined;
-  req.body.createdAt = undefined;
-  req.body.updatedAt = undefined;
-
-  const { isRejected } = await Categoria.update(
-    req.body,
-    {
-      where: {
-        id
-      }
+    if( !allow ) {
+      return res.status(403).json({ error: 'Acesso negado' });
     }
-  );
 
-  if( isRejected ){
-    return res.status(400).json({ error: 'Não foi possível atualizar a categoria' });
+    const categoria = await Categoria.findByPk( id );
+    if( !categoria ) {
+      return res.status(400).json({ error: 'Está categoria não existe' });
+    }
+
+    req.body.id = undefined;
+    req.body.createdAt = undefined;
+    req.body.updatedAt = undefined;
+
+    const { isRejected } = await Categoria.update(
+      req.body,
+      {
+        where: {
+          id
+        }
+      }
+    );
+
+    if( isRejected ){
+      return res.status(400).json({ error: 'Não foi possível atualizar a categoria' });
+    }
+
+    const result = await Categoria.findByPk( id );
+
+    return res.json( result );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  const result = await Categoria.findByPk( id );
-
-  return res.json( result );
 }
 
 const router = express.Router();

3b42d23844ccc10f804aa04cbd22b177464dcee3
rafae9000 <42677838+rafae9000@users.noreply.github.com>
2022-11-17 17:33:51 -0300

    Melhoria das rotas de municipio
    Caso ocorresse algum comportamento inesperado em alguma das rotas, não conseguir conectar com o banco por exemplo, ela não era tratado por uma clausula try/catch, o que fazia a api parar de funcionar.
    
    Agora quando uma falha inesperada acontecer, é enviado uma reposta para o web: "Algum problema inesperado ocorreu nesta rota da api".

--- src/controllers/MunicipioController.js
+++ src/controllers/MunicipioController.js
@@ -10,171 +10,220 @@ const allowFunction = require('../util/allowFunction');
 const router = express.Router();
 
 index = async ( req, res ) => {
-  const municipios = await Municipio.findAll({
-    order: [
-      [ 'ativo', 'desc' ],
-      [ 'nome', 'asc' ], 
-      [ 'codigo', 'asc' ]
-    ]
-  });
+  try{
+    const municipios = await Municipio.findAll({
+      order: [
+        [ 'ativo', 'desc' ],
+        [ 'nome', 'asc' ], 
+        [ 'codigo', 'asc' ]
+      ]
+    });
 
-  res.json( municipios );
+    res.json( municipios );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 getCityById = async ( req, res ) => {
-  const { id } = req.params;
+  try{
+    const { id } = req.params;
 
-  const municipio = await Municipio.sequelize.query(
-    'SELECT ' +
-      'm.*, ' +
-      'rs.id AS regional_saude_id, ' +
-      'e.id AS estado_id, ' +
-      'r.id AS regiao_id, ' + 
-      'p.id AS pais_id  ' +
-    'FROM  ' +
-      'municipios as m  ' +
-      'JOIN "regionais_saude" as rs ON( m.regional_saude_id = rs.id ) ' +
-      'JOIN estados as e ON( rs.estado_id = e.id ) ' +
-      'JOIN regioes as r ON( e.regiao_id = r.id ) ' +
-      'JOIN paises as p ON( r.pais_id = p.id ) ' +
-    'WHERE ' +
-      'm.id = $1', 
-    {
-      bind: [id],
-      logging: console.log,
-      plain: true,
-      model: Municipio,
-      mapToModel: true
+    const municipio = await Municipio.sequelize.query(
+      'SELECT ' +
+        'm.*, ' +
+        'rs.id AS regional_saude_id, ' +
+        'e.id AS estado_id, ' +
+        'r.id AS regiao_id, ' + 
+        'p.id AS pais_id  ' +
+      'FROM  ' +
+        'municipios as m  ' +
+        'JOIN "regionais_saude" as rs ON( m.regional_saude_id = rs.id ) ' +
+        'JOIN estados as e ON( rs.estado_id = e.id ) ' +
+        'JOIN regioes as r ON( e.regiao_id = r.id ) ' +
+        'JOIN paises as p ON( r.pais_id = p.id ) ' +
+      'WHERE ' +
+        'm.id = $1', 
+      {
+        bind: [id],
+        logging: console.log,
+        plain: true,
+        model: Municipio,
+        mapToModel: true
+      }
+    );
+
+    // const municipio = await Municipio.findByPk( id, {
+    //   include: {
+    //       association: 'regional'
+    //   },
+    //   attributes: {
+    //     exclude: [ 'regionalSaude_id' ]
+    //   }
+    // });
+
+    if( !municipio ) {
+      return res.status(400).json({ error: 'Município não encontrado' });
     }
-  );
 
-  // const municipio = await Municipio.findByPk( id, {
-  //   include: {
-  //       association: 'regional'
-  //   },
-  //   attributes: {
-  //     exclude: [ 'regionalSaude_id' ]
-  //   }
-  // });
-
-  if( !municipio ) {
-    return res.status(400).json({ error: 'Município não encontrado' });
+    return res.json( municipio );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  return res.json( municipio );
 }
 
 getCityByRegionalHealth = async ( req, res ) => {
-  const { regionalSaude_id } = req.params;
+  try{
+    const { regionalSaude_id } = req.params;
 
-  const regional = RegionalSaude.findByPk( regionalSaude_id );
+    const regional = RegionalSaude.findByPk( regionalSaude_id );
 
-  if( !regional ) {
-    return res.status(400).json({ error: "Regional não existe" });
+    if( !regional ) {
+      return res.status(400).json({ error: "Regional não existe" });
+    }
+
+    const municipios = await Municipio.findAll({
+      where: {
+        regional_saude_id: regionalSaude_id
+      },
+      order: [[ 'nome', 'asc' ]]
+    });
+
+    return res.json( municipios );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  const municipios = await Municipio.findAll({
-    where: {
-      regional_saude_id: regionalSaude_id
-    },
-    order: [[ 'nome', 'asc' ]]
-  });
-
-  return res.json( municipios );
 }
 
 listUser = async (req, res) => {
-  const { municipio_id } = req.params;
+  try{
+    const { municipio_id } = req.params;
 
-  const municipioFind = await Municipio.findByPk(municipio_id);
+    const municipioFind = await Municipio.findByPk(municipio_id);
 
-  if( !municipioFind ) {
-    return res.status(400).json({ error: 'Município não encontrado' });
-  }
-
-  const user = await Usuario.findByPk( req.userId, {include: { association: 'municipio' }} );
-
-  if( !user ) {
-    return res.status(401).json({ error: 'Usuário não existe' });
-  }
-
-  if( user.tipoPerfil !== "C" || user.municipio.id !== parseInt(municipio_id) ) {
-    return res.status(403).json({ error: 'Acesso negado' });
-  }
-
-  const municipio = await Municipio.findByPk(municipio_id, {
-    include: {
-      association: 'usuarios'
+    if( !municipioFind ) {
+      return res.status(400).json({ error: 'Município não encontrado' });
     }
-  });
 
-  return res.json(municipio.usuarios);
+    const user = await Usuario.findByPk( req.userId, {include: { association: 'municipio' }} );
+
+    if( !user ) {
+      return res.status(401).json({ error: 'Usuário não existe' });
+    }
+
+    if( user.tipoPerfil !== "C" || user.municipio.id !== parseInt(municipio_id) ) {
+      return res.status(403).json({ error: 'Acesso negado' });
+    }
+
+    const municipio = await Municipio.findByPk(municipio_id, {
+      include: {
+        association: 'usuarios'
+      }
+    });
+
+    return res.json(municipio.usuarios);
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 store = async (req, res) => {
-  const { codigo, nome, regionalSaude_id } = req.body;
+  try{
+    const { codigo, nome, regionalSaude_id } = req.body;
 
-  const allow = await allowFunction( req.userId, 'manter_municipio' );
-  if( !allow ) {
-    return res.status(403).json({ error: 'Acesso negado' });
+    const allow = await allowFunction( req.userId, 'manter_municipio' );
+    if( !allow ) {
+      return res.status(403).json({ error: 'Acesso negado' });
+    }
+
+    const municipio = await Municipio.create({ 
+      codigo,
+      nome,
+      regional_saude_id: regionalSaude_id,
+      ativo: 1
+    });
+
+    return res.status(201).json(municipio);
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  const municipio = await Municipio.create({ 
-    codigo,
-    nome,
-    regional_saude_id: regionalSaude_id,
-    ativo: 1
-  });
-
-  return res.status(201).json(municipio);
 }
 
 update = async (req, res) => {
-  const userId = req.userId;
-  const { id } = req.params;
+  try{
+    const userId = req.userId;
+    const { id } = req.params;
 
-  const allow = await allowFunction( req.userId, 'manter_municipio' );
-  if( !allow ) {
-    return res.status(403).json({ error: 'Acesso negado' });
-  }
-
-  const municipio = await Municipio.findByPk( id );
-  if( !municipio ) {
-    return res.status(400).json({ error: 'Município não existe' });
-  }
-
-  req.body.id = undefined;
-  req.body.createdAt = undefined;
-  req.body.updatedAt = undefined;
-
-  const { isRejected } = await Municipio.update(
-    req.body,
-    {
-      where: {
-        id
-      }
+    const allow = await allowFunction( req.userId, 'manter_municipio' );
+    if( !allow ) {
+      return res.status(403).json({ error: 'Acesso negado' });
     }
-  );
 
-  if( isRejected ){
-    return res.status(400).json({ error: 'Não foi possível atualizar o município' });
+    const municipio = await Municipio.findByPk( id );
+    if( !municipio ) {
+      return res.status(400).json({ error: 'Município não existe' });
+    }
+
+    req.body.id = undefined;
+    req.body.createdAt = undefined;
+    req.body.updatedAt = undefined;
+
+    const { isRejected } = await Municipio.update(
+      req.body,
+      {
+        where: {
+          id
+        }
+      }
+    );
+
+    if( isRejected ){
+      return res.status(400).json({ error: 'Não foi possível atualizar o município' });
+    }
+
+    const result = await Municipio.findByPk( id );
+
+    return res.json( result );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
   }
-
-  const result = await Municipio.findByPk( id );
-
-  return res.json( result );
 }
 
 destroy = async ( req, res ) => {
-  const { id } = req.params;
+  try{
+    const { id } = req.params;
 
-  const result = await Municipio.destroy({
-    where: {
-      id
-    }
-  });
+    const result = await Municipio.destroy({
+      where: {
+        id
+      }
+    });
 
-  return res.json( result );
+    return res.json( result );
+  } catch (error) {
+    return res.status( 400 ).send( { 
+      status: 'unexpected error',
+      mensage: 'Algum problema inesperado ocorreu nesta rota da api',
+    } );
+  }
 }
 
 router.use(authMiddleware);

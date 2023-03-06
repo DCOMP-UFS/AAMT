2d91ecb777ced7a3810a3d8449846ec5242fe9f7
rafae9000 <42677838+rafae9000@users.noreply.github.com>
2023-03-06 12:33:14 -0300

    Melhoria na pagina de acesso do boletim por ativ.
    Antes só era possível acessar os os boletins por atividade das atividades que pertenciam ao ciclo que estava aberto no momento
    
    Agora também é possível ver o boletim por atividade das atividades de ciclos que já foram encerrados

--- src/pages/Relatorio/Atividade/index.js
+++ src/pages/Relatorio/Atividade/index.js
@@ -1,29 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { FaChartLine } from 'react-icons/fa';
-import { Row, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import { dataToStringBr } from '../../../config/function';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // ACTIONS
 import { changeSidebar } from '../../../store/Sidebar/sidebarActions';
-import { getCicloAbertoRequest } from '../../../store/Ciclo/cicloActions';
+import { getCicloAbertoRequest, getOpenAndFinishedCyclesRequest } from '../../../store/Ciclo/cicloActions';
 import { getResponsabilityActivitiesRequest } from '../../../store/Atividade/atividadeActions';
 
 // STYLES
 import { Container } from './styles';
 import { PageIcon, PageHeader } from '../../../styles/util';
 
-export const RelatorioAtividade = ({ atividades, ciclo, usuario, ...props }) => {
+//COMPONENTS
+import { CycleSelector } from '../../../components/CycleSelector';
+
+export const RelatorioAtividade = ({ atividades, ciclos, usuario, ...props }) => {
+
+  const [ cicloSelecionado, setCicloSelecionado ] = useState(null);
+  const [ optionCiclos, setOptionCiclos ] = useState({});
+
   useEffect(() => {
     props.changeSidebar( "relatorio", "rlt_porAtividade" );
-    props.getCicloAbertoRequest( usuario.municipio.regional.id );
+    //props.getCicloAbertoRequest( usuario.municipio.regional.id );
+    props.getOpenAndFinishedCyclesRequest(usuario.municipio.regional.id)
   }, []);
 
   useEffect(() => {
-    if( Object.entries( ciclo ).length > 0 ) {
-      props.getResponsabilityActivitiesRequest( usuario.id, ciclo.id );
+    const options = ciclos.map( (ciclo) => {
+      let current_date = new Date();
+      let dataInicio = new Date( ciclo.dataInicio );
+      let dataFim = new Date( ciclo.dataFim );
+      current_date.setHours(0,0,0,0);
+      dataInicio.setHours(0,0,0,0);
+      dataFim.setHours(0,0,0,0);
+
+      const periodo_ciclo = " -> de "+dataToStringBr(ciclo.dataInicio)+" até "+dataToStringBr(ciclo.dataFim)
+
+      if( dataInicio <= current_date && dataFim >= current_date )
+        setCicloSelecionado({ 
+          value: ciclo.id, 
+          label: `${ ciclo.ano }.${ ciclo.sequencia }`+periodo_ciclo, 
+          dataInicio: ciclo.dataInicio, 
+          dataFim: ciclo.dataFim });
+
+      return (
+        { 
+          value: ciclo.id, 
+          label: `${ ciclo.ano }.${ ciclo.sequencia }`+periodo_ciclo, 
+          dataInicio: ciclo.dataInicio, 
+          dataFim: ciclo.dataFim 
+        }
+      );
+    });
+
+    setOptionCiclos(options);
+  }, [ ciclos ]);
+
+  useEffect(() => {
+    if( cicloSelecionado != null) {
+      props.getResponsabilityActivitiesRequest( usuario.id, cicloSelecionado.value );
     }
-  }, [ ciclo ]);
+  }, [ cicloSelecionado ]);
 
   const submit = (atividade_id) => {
     window.location = window.location.origin.toString() + '/relatorio/atividade/' + atividade_id;
@@ -38,32 +78,57 @@ export const RelatorioAtividade = ({ atividades, ciclo, usuario, ...props }) => 
         </h3>
       </PageHeader>
 
+      <CycleSelector 
+        optionCiclos={optionCiclos} 
+        cicloSelecionado={cicloSelecionado} 
+        setCicloSelecionado={ (e) => setCicloSelecionado(e) } />
+
       <section className="card-list">
         <article className="row">
           {
-            atividades.map( atividade => (
-              <Col key={ 'atv-' + atividade.id } md="4" onClick={() => submit(atividade.id)}>
-                <div className="card2 atividade">
-                  <div className="card2-header">
-                    <h3 className="title">{ atividade.metodologia.sigla }</h3>
+            (() => {
+              if(props.searchResponsabilityActivities){
+                return (
+                  <div style={{ marginTop: "25%", marginLeft: "50%" }}>
+                    <CircularProgress color="inherit" />
                   </div>
-                  <div className="card2-body">
-                    <div className="form-group">
-                      <label style={{ fontWeight: 'bold' }}>Código</label>
-                      <span>{ atividade.id }</span>
-                    </div>
-                    <div className="form-group">
-                      <label style={{ fontWeight: 'bold' }}>Objetivo</label>
-                      <span>{ atividade.objetivo.descricao }</span>
-                    </div>
-                    <div className="form-group mb-0">
-                      <label style={{ fontWeight: 'bold' }}>Data de Início</label>
-                      <span>{ dataToStringBr( ciclo.dataInicio ) }</span>
-                    </div>
+                )
+              }
+              else if(atividades.length == 0) {
+                return (
+                  <div style={{ marginTop: "10%", marginLeft: "30%" }}>
+                    <h3>Não foi encontrada nenhuma atividade</h3>
                   </div>
-                </div>
-              </Col>
-            ) )
+                )
+              }
+              else{
+                return(
+                  atividades.map( atividade => (
+                    <Col key={ 'atv-' + atividade.id } md="4" onClick={() => submit(atividade.id)}>
+                      <div className="card2 atividade">
+                        <div className="card2-header">
+                          <h3 className="title">{ atividade.metodologia.sigla }</h3>
+                        </div>
+                        <div className="card2-body">
+                          <div className="form-group">
+                            <label style={{ fontWeight: 'bold' }}>Código</label>
+                            <span>{ atividade.id }</span>
+                          </div>
+                          <div className="form-group">
+                            <label style={{ fontWeight: 'bold' }}>Objetivo</label>
+                            <span>{ atividade.objetivo.descricao }</span>
+                          </div>
+                          <div className="form-group mb-0">
+                            <label style={{ fontWeight: 'bold' }}>Data de Início</label>
+                            <span>{ dataToStringBr( cicloSelecionado.dataInicio ) }</span>
+                          </div>
+                        </div>
+                      </div>
+                    </Col>
+                  ) )
+                )
+              }
+            })()
           }
         </article>
       </section>
@@ -73,14 +138,16 @@ export const RelatorioAtividade = ({ atividades, ciclo, usuario, ...props }) => 
 
 const mapStateToProps = state => ( {
   usuario   : state.appConfig.usuario,
-  ciclo     : state.ciclo.ciclo,
-  atividades: state.atividade.atividades
+  ciclos     : state.ciclo.ciclos,
+  atividades: state.atividade.atividades,
+  searchResponsabilityActivities: state.atividade.searchResponsabilityActivities
 } );
 
 const mapDispatchToProps = {
   changeSidebar,
   getCicloAbertoRequest,
-  getResponsabilityActivitiesRequest
+  getResponsabilityActivitiesRequest,
+  getOpenAndFinishedCyclesRequest
 }
 
 export default connect(
